9ea9e40ca0e90f137e0eeb14a2106f66efd8aba8
yvdriess <yvdriess@vub.ac.be>
2015-03-09 15:10:51 +0100

    minor change

--- build_xmt.sh
+++ build_xmt.sh
@@ -1,1 +1,1 @@
-cc -pl bwa.pl -i4 -DXMT -I../zlib-1.2.8/ -o bwa main.c utils.c libz.pl bwaseqio.c bntseq.c bwt.c bwa.c kstring.c bamlite.c QSufSort.c is.c bwt_gen.c bwtindex.c bwtgap.c bwtaln.c -lm
+cc -pl bwa.pl -i4 -xmt2 -DXMT -I../zlib-1.2.8/ -o bwa main.c utils.c libz.pl bwaseqio.c bntseq.c bwt.c bwa.c kstring.c bamlite.c QSufSort.c is.c bwt_gen.c bwtindex.c bwtgap.c bwtaln.c -lm
